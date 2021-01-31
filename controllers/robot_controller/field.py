8b1a3ef13ac465bc5494f601be518ae51238f960
Ethan Mak <ethan.mak100@gmail.com>
2021-01-30 19:34:18 -0500

    Adds raytracing support

--- controllers/robot_controller/field.py
+++ controllers/robot_controller/field.py
@@ -1,4 +1,5 @@
 import enum
+from .utils import *
 
 class Color(enum.Enum):
     GREEN = (0, 255, 0)
@@ -6,11 +7,23 @@ class Color(enum.Enum):
     UNKNOWN = (88, 92, 99)
 
 class Field:
-    walls = [(-1.2, -1.2), (1.2, 1.2)]
+    walls = [np.array([-1.2, -1.2]), np.array([-1.2, 1.2]), np.array([1.2, 1.2]), np.array([1.2, -1.2])]
 
     @staticmethod
-    def distance_to_wall(self, position, rotation):
-        return 0
+    def distance_to_wall(origin, rotation):
+        direction = degree_to_vector(rotation)
+        v3 = np.array([-direction[1], direction[0]])
+        for i in range(-1, len(Field.walls) - 1):
+            v1 = origin - Field.walls[i]
+            v2 = Field.walls[i+1] - Field.walls[i]
+            dot = np.dot(v2, v3)
+            if np.isclose(dot, 0):
+                continue
+            t1 = np.cross(v2, v1) / dot
+            t2 = np.dot(v1, v3) / dot
+            if t1 >= 0.0 and 0.0 <= t2 <= 1.0:
+                return np.linalg.norm(origin + t1 * direction - origin)
+        return -2
 
     def __init__(self):
         self.field = {}
--- controllers/util_test.py
+++ controllers/util_test.py
@@ -1,7 +1,9 @@
 from robot_controller.utils import *
+from robot_controller.field import Field
 import numpy as np
 
 if __name__ == '__main__':
     x = np.array([1, -1])
     y = np.array([-1, 1])
     print(vector_degree(y - x))
+    print(Field.distance_to_wall(x, 90))
